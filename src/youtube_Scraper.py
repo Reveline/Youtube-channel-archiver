b3f3c40f9778b4904377aa39824aad252ce58acc
Reveline <jacortinez18@gmail.com>
2026-05-02 17:48:48 -0400

    Initial commit: Project structure

---
+++ src/youtube_Scraper.py
@@ -0,0 +1,14 @@
+from selenium import webdriver # Este modulo te permite controlar el navegador web para interactuar con la página de YouTube
+from selenium.webdriver.common.keys import Keys # Este modulo te permite usar las teclas del teclado para interactuar con la página
+from selenium.webdriver.common.by import By # Este modulo te permite usar los selectores de CSS para encontrar elementos en la página
+from time import sleep # Este modulo realiza pausas en la ejecución del programa para esperar a que se cargue la página o los elementos
+
+import yt_dlp
+
+
+'''
+def c():
+    driver = webdriver.Chrome() # Inica la sesion
+
+driver.quit() # Termina la sesion
+'''
